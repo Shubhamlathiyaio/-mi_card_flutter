369f2c09cf5b582c039e3afd255de54017b1058d
Divol3tech <divol3.tech@gmail.com>
2025-03-21 17:25:00 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(MyApp());
 }
 
+const PI = 3.14159;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,13 +15,56 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(20),
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: Text("Hello"))),
+            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.all(Radius.circular(5))),padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 1"),
+                ),
+                Container(
+                  decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.all(Radius.circular(5))),padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 2"),
+                ),
+                Container(
+                  decoration: BoxDecoration(color: Colors.yellow,borderRadius: BorderRadius.all(Radius.circular(5))),padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 3"),
+                ),
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.all(Radius.circular(5))),padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 1"),
+                  transform: Matrix4.rotationZ(PI / 4),
+                ),
+                Container(
+                  decoration: BoxDecoration(color: Colors.yellow,borderRadius: BorderRadius.all(Radius.circular(5))),padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 2"),transform: Matrix4.rotationZ(PI / 2),
+                ),
+                Container(
+                  decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.all(Radius.circular(5))),padding: EdgeInsets.all(5),
+                  height: 100,
+                  width: 100,
+                  child: Text("Container 3"),transform: Matrix4.rotationZ(PI / 1),
+                ),
+              ],
+            ),
+          ],
+        )),
       ),
     );
   }
